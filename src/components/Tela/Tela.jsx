2d3b324a05861b2a13b7ba84993ec2d2396dda71
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2024-10-17 11:03:19 -0300

    feat: tela menu

--- src/components/Tela/Tela.jsx
+++ src/components/Tela/Tela.jsx
@@ -1,26 +1,53 @@
-import Card  from '../Card/Card'
-import Menu from '../Menu'
-import React from 'react';
+import Card from '../Card/Card';
+import Menu from '../Menu';
+import React, { useState } from 'react';
 import Clock from '../Clock/Clock';
-import './Tela.css'
+import './Tela.css';
+import Perfil from './iconeperfil.png';
 
-const Tela=()=>{
+const Tela = () => {
+    const [imagem, setImagem] = useState(null);
 
-        
-    return(
+    const handleImageChange = (event) => {
+        const file = event.target.files[0]; // Pega o primeiro arquivo selecionado
+        if (file) {
+            const reader = new FileReader(); // Cria um FileReader
+            reader.onloadend = () => {
+                setImagem(reader.result); // Atualiza o estado com a imagem
+            };
+            reader.readAsDataURL(file); // Lê o arquivo como uma URL de dados
+        }
+    };
+
+    return (
         <div className='mobile-screen'>
             <div className='cell-panel'>
                 <Clock />
             </div>
+
             <div className='painel-menu'>
-            <Menu />
-            <h1 className='title-tela'>Bicicletas</h1>
-            <div className='perfil'>
-                <img src="" alt="" />
-                <div className='circle'></div>
+                <Menu />
+                <h1 className='title-tela'>Bicicletas</h1>
+                
+                <div className='perfil'>
+                    <input 
+                        type='file' 
+                        id='filImage' 
+                        name='filmage' 
+                        accept='image/*'  
+                        onChange={handleImageChange} 
+                        style={{ display: 'none' }} // Oculte o input
+                    />
+                    <img 
+                        src={imagem || Perfil} 
+                        alt="Selecione uma imagem" 
+                        id="imgPhoto" 
+                        onClick={() => document.getElementById('filImage').click()} // Adicione o evento de clique
+                    />
+                    <div className='circle'></div>
+                </div>
             </div>
-            </div>
-            
+
             <Card />
         </div>
     );
