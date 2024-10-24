b8a099b1284e0ab4ae5a0de4e674a04adcb497c9
Luciano Silva <lcnsilvajf@gmail.com>
2024-10-24 20:19:26 -0300

    feat: add router dom

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import MenuLateral from "../../components/Menu";
+import './style.css'
+
+const Home = () => {
+    return(
+        <div>
+            <MenuLateral />
+            <h1>Home</h1>
+        </div>    
+        )
+}
+
+export default Home;
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import MenuLateral from "../../components/Menu";
+import './style.css'
+
+const Login = () => {
+    return (
+        <div>
+            <MenuLateral />
+            <h1>Login</h1>
+        </div>    
+    )
+}
+
+export default Login;
