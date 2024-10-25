f706ea1e9fd7d7a987516fc22a781db370dae4c1
Luciano Silva <lcnsilvajf@gmail.com>
2024-10-25 13:55:15 -0300

    feat: add login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,21 @@
-import MenuLateral from "../../components/Menu";
 import './style.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <MenuLateral />
-            <h1>Login</h1>
+        <div className='card'>
+            <h1>Entrar na sua conta</h1>
+            <input placeholder='Email' type='email'/>
+            <input placeholder='Senha' type='password'/>
+            <div className='password__container'>
+                <div className='remember__container'>
+                    <input type='radio' id='rememberMe'/>
+                    <label htmlFor="rememberMe">Lembrar-me</label>
+                </div>
+                <Link to='/recuperar' className='link'>Esqueceu a senha?</Link>
+            </div>
+            <button className='button__signIn'>Entrar com email</button>
+            <p>Não tem uma conta? <Link to='/registro' className='link'>Registre-se</Link></p>
         </div>    
     )
 }
